a91f4697c8d112a94beab843106be01ca8505879
ICEDRHINO23 <abinavft@gmail.com>
2026-06-29 20:58:59 +0530

    Update Supabase anon key in supabase.js

--- database/supabase.js
+++ database/supabase.js
@@ -2,8 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const SUPABASE_URL = "https://tdrkhigqryojycordmja.supabase.co";
 
-const SUPABASE_ANON_KEY =
-"PASTE_YOUR_FULL_sb_publishable_KEY_HERE";
+const SUPABASE_ANON_KEY = "sb_publishable_H3j9OiFj9ASHLJQmc1GGGQ_kkFY_l5h";
 
 export const supabase = createClient(
     SUPABASE_URL,
