b7fc09bc5b474563cb4a4e2cebec20986f66df73
ICEDRHINO23 <abinavft@gmail.com>
2026-07-04 00:22:08 +0530

    Add filterData utility function for data filtering

--- utils/filters.js
+++ utils/filters.js
@@ -1,1 +1,19 @@
+/* ==========================================================
+   FILTER UTILITY
+========================================================== */
 
+export function filterData(data, filters) {
+
+    return data.filter(item => {
+
+        return Object.keys(filters).every(key => {
+
+            if (!filters[key]) return true;
+
+            return String(item[key]) === String(filters[key]);
+
+        });
+
+    });
+
+}
